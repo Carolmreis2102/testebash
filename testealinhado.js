202f8f16fac0ca55a097514283721ecccc08a691
Carolmreis2102 <anacarolinareis2102@hotmail.com>
2022-03-24 16:23:19 -0300

    aula teste

---
+++ testealinhado.js
@@ -0,0 +1,8 @@
+let nota = 9
+if(nota >=8){
+    console.log('Aprovado!')
+} else if(nota <5){
+    console.log('reprovado')
+}else{
+    console.log('Aprovado pelo conselho')
+}
---
+++ testeswutch.js
@@ -0,0 +1,29 @@
+console.log ('Estações do ano')
+let mes = 'Fevereiro'
+
+switch(mes){
+    case 'Janeiro':
+    case 'Fevereiro':
+    case 'Março':
+     console.log('Verão')
+     break
+
+     case 'Abril':
+     case 'Maio':
+     case 'Junho':
+         console.log('Outono')
+         break
+    
+     case 'Julho':
+     case 'Agosto':
+     case 'Setembro':
+         console.log('Inverno')
+         break
+        
+     case 'Outubro':
+     case 'Novembro':
+     case 'Dezembro':
+         console.log('Primavera')
+         break
+
+}
